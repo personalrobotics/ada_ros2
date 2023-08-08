28510d5328139dbf2ba92b59cf31b131041df751
Haya Bolotski <hayabean@gmail.com>
2023-08-08 11:11:20 -0700

    [WIP] worked on IMU jointstate publisher node

--- ada_imu/ada_imu/imu_jointstate_publisher.py
+++ ada_imu/ada_imu/imu_jointstate_publisher.py
@@ -1,25 +1,53 @@
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import JointState
 
-from std_msgs.msg import String
+import numpy as np
 
+# calibration values (level)
+cX = -1003.4183333333335
+cY = -24.198333333333334
+cZ = 38.275000000000006
+
+#calibration values (tilt)
+tX = -978.8416666666667
+tY = -232.50333333333336
+tZ = 40.96
+
+main_calib_vector = np.array([cX, cY, cZ])
+tilt_calib_vector = np.array([tX, tY, tZ])
+
+#calculate the direction vector at the beginning
+#vector for determining in which direction the IMU is being rotated
+direction_vector = double_cross
 
 
 class IMUJointstatePublisher(Node):
 
     def __init__(self):
-        super().__init__('imu_jointstate_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        super().__init__('IMU_jointstate_publisher')
+        self.publisher_ = self.create_publisher(JointState, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = JointState()
+        msg.name = []
+        msg.position = []
+        msg.velocity =  []
+        msg.effort = []
+
+        msg.name.append('IMU Joint')
+        msg.position.append(1.0)
+        msg.velocity.append(2.0)
+
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = 'root'
+
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.get_logger().info('Publishing: "%s" \n' % msg)
+
 
 
 def main(args=None):
@@ -38,3 +66,15 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+#returns angle in radians between vectors v1 and v2
+def angle_between(v1, v2):
+    #get unit vectors
+    v1_u = v1 / np.linalg.norm(v1)
+    v2_u = v2 / np.linalg.norm(v2)
+
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def angle_direction
