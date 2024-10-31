93230997c702bce1411872b50d74e6447e5b628b
Amal Nanavati <amaln@uw.edu>
2024-10-30 17:07:23 -0700

    Add an auto-camera calibration script (#48)
    * [WIP] Adding auto camera calib procedure
    
    * [WIP] Adding camera calibration script
    
    * [WIP] added cartesian motion, need to add charucoboard code
    
    * [WIP] almost done, need to test in-person
    
    * Mostly working but for error computation
    
    * Fixed bug in calculating transformation error
    
    * Change default calib to autocalib
    
    * [WIP] moved to separate package, yet to test
    
    * Finalized TODOs
    
    * Changes from final testing
    
    * Reject calibrations that are too far from the manual reference
    
    * Make teh hardcoded path consistent with lovelace
    
    * Fix off-center issue (#49)
    
    Co-authored-by: Ethan K. Gordon <ekgordon@cs.washington.edu>
    
    * Update forque centering given updated URDF
    
    * Small updates to camera calibration scripts
    
    ---------
    
    Co-authored-by: Amal Nanavati <amalnanavati@gmail.com>
    Co-authored-by: Ethan K. Gordon <ekgordon@cs.washington.edu>

---
+++ ada_calibrate_camera/ada_calibrate_camera/publish_camera_extrinsics_node.py
@@ -0,0 +1,100 @@
+# !/usr/bin/env python3
+"""
+This module contains a rclpy ROS2 node that takes in parameters for the translation
+and rotation (quaternion) as well as the frame_id's and publishes the requested transform.
+"""
+
+# Standard imports
+
+# Third-party imports
+from geometry_msgs.msg import TransformStamped
+import rclpy
+from rclpy.node import Node
+from scipy.spatial.transform import Rotation as R
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+
+# Local imports
+
+
+class PublishCameraExtrinsics(Node):
+    """
+    This class defines a ROS2 node that takes in parameters for the translation
+    and rotation (quaternion) as well as the frame_id's and publishes the requested transform.
+    """
+
+    def __init__(self):
+        """
+        Initializes the node and sets up the timer to publish the transform
+        """
+        super().__init__("publish_camera_extrinsics")
+        self.declare_parameters(
+            namespace="",
+            parameters=[
+                ("frame_id", "j2n6s200_end_effector"),
+                ("child_frame_id", "camera_color_optical_frame"),
+                ("x", 0.0),
+                ("y", 0.0),
+                ("z", 0.0),
+                ("roll", 0.0),
+                ("pitch", 0.0),
+                ("yaw", 0.0),
+                ("rate_hz", 10.0),
+            ],
+        )
+
+        self.tf_broadcaster = StaticTransformBroadcaster(self)
+
+        timer_interval = (
+            1.0 / self.get_parameter("rate_hz").get_parameter_value().double_value
+        )
+        self.timer = self.create_timer(timer_interval, self.publish_transform)
+
+    def publish_transform(self):
+        """
+        Publishes the transform from the frame_id to the child_frame_id
+        """
+        transform = TransformStamped()
+        transform.header.stamp = self.get_clock().now().to_msg()
+        transform.header.frame_id = (
+            self.get_parameter("frame_id").get_parameter_value().string_value
+        )
+        transform.child_frame_id = (
+            self.get_parameter("child_frame_id").get_parameter_value().string_value
+        )
+        transform.transform.translation.x = (
+            self.get_parameter("x").get_parameter_value().double_value
+        )
+        transform.transform.translation.y = (
+            self.get_parameter("y").get_parameter_value().double_value
+        )
+        transform.transform.translation.z = (
+            self.get_parameter("z").get_parameter_value().double_value
+        )
+        quat = R.from_euler(
+            "ZYX",
+            [
+                self.get_parameter("yaw").get_parameter_value().double_value,
+                self.get_parameter("pitch").get_parameter_value().double_value,
+                self.get_parameter("roll").get_parameter_value().double_value,
+            ],
+        ).as_quat()
+        transform.transform.rotation.x = quat[0]
+        transform.transform.rotation.y = quat[1]
+        transform.transform.rotation.z = quat[2]
+        transform.transform.rotation.w = quat[3]
+
+        self.tf_broadcaster.sendTransform(transform)
+
+
+def main():
+    """
+    The main function to create and spin the node
+    """
+    rclpy.init()
+    node = PublishCameraExtrinsics()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ada_calibrate_camera/setup.py
@@ -0,0 +1,52 @@
+from glob import glob
+import os
+from setuptools import find_packages, setup
+
+package_name = "ada_calibrate_camera"
+
+data_files = [
+    ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+    ("share/" + package_name, ["package.xml"]),
+    # Include all launch files.
+    (
+        os.path.join("share", package_name, "launch"),
+        glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+    ),
+    # Include all config files.
+    (
+        os.path.join("share", package_name, "config"),
+        glob(os.path.join("config", "*.yaml")),
+    ),
+    (
+        os.path.join("share", package_name, "config/calibs"),
+        glob(os.path.join("config/calibs", "*.yaml")),
+    ),
+]
+for path in os.listdir("data"):
+    if os.path.isdir(os.path.join("data", path)):
+        data_files.append(
+            (
+                os.path.join("share", package_name, "data", path),
+                glob(os.path.join("data", path, "*.[pn][np][gz]")),
+            )
+        )
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=data_files,
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Amal Nanavati",
+    maintainer_email="amaln@cs.washington.edu",
+    description=("This package contains a script to calibrate ADA's camera."),
+    license="BSD-3-Clause",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "calibrate_camera = ada_calibrate_camera.calibrate_camera_node:main",
+            "publish_camera_extrinsics = ada_calibrate_camera.publish_camera_extrinsics_node:main",
+        ],
+    },
+)
