4ee7f489d83296d85c4405362175d1b457670ce2
Jose Jaime <jose33jaime@gmail.com>
2025-03-26 20:46:32 -0700

    Implement ADA Xacro macro

--- ada_description/launch/view_ada.launch.py
+++ ada_description/launch/view_ada.launch.py
@@ -60,7 +60,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_file",
-            default_value="ada.xacro",
+            default_value="ada_standalone.xacro",
             description="URDF/XACRO description file with the robot.",
         )
     )
