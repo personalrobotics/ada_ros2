67b09c8d8d600178f20c4feed28d9e60431c8006
Jose Jaime <jose33jaime@gmail.com>
2025-03-30 14:28:47 -0700

    Add ada namespace into moveit.rviz and moveit_controllers.yaml. Perform required remapping to namespaced topics

--- ada_moveit/launch/ada_moveit.launch.py
+++ ada_moveit/launch/ada_moveit.launch.py
@@ -25,6 +25,7 @@ from launch.substitutions import (
     TextSubstitution,
     PythonExpression,
 )
+from launch_ros.parameter_descriptions import ParameterValue
 
 from launch_ros.actions import Node, PushRosNamespace
 
@@ -129,6 +130,55 @@ def generate_launch_description():
         ),
     )
 
+    ld.add_action(
+        DeclareBooleanLaunchArg("allow_trajectory_execution", default_value=True)
+    )
+    ld.add_action(
+        DeclareBooleanLaunchArg("publish_monitored_planning_scene", default_value=True)
+    )
+    # load non-default MoveGroup capabilities (space separated)
+    ld.add_action(
+        DeclareLaunchArgument(
+            "capabilities",
+            default_value=moveit_config.move_group_capabilities["capabilities"],
+        )
+    )
+    # inhibit these default MoveGroup capabilities (space separated)
+    ld.add_action(
+        DeclareLaunchArgument(
+            "disable_capabilities",
+            default_value=moveit_config.move_group_capabilities["disable_capabilities"],
+        )
+    )
+
+    # do not copy dynamics information from /joint_states to internal robot monitoring
+    # default to false, because almost nothing in move_group relies on this information
+    ld.add_action(DeclareBooleanLaunchArg("monitor_dynamics", default_value=False))
+
+    should_publish = LaunchConfiguration("publish_monitored_planning_scene")
+
+    move_group_configuration = {
+        "publish_robot_description_semantic": True,
+        "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
+        # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
+        "capabilities": ParameterValue(
+            LaunchConfiguration("capabilities"), value_type=str
+        ),
+        "disable_capabilities": ParameterValue(
+            LaunchConfiguration("disable_capabilities"), value_type=str
+        ),
+        # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
+        "publish_planning_scene": should_publish,
+        "publish_geometry_updates": should_publish,
+        "publish_state_updates": should_publish,
+        "publish_transforms_updates": should_publish,
+        "monitor_dynamics": False,
+    }
+
+    move_group_params = [
+        moveit_config.to_dict(),
+        move_group_configuration,
+    ]
 
     actions = [
         PushRosNamespace("ada"),
@@ -148,7 +198,16 @@ def generate_launch_description():
             remappings=[('/joint_states', '/ada/combined_joint_states')],
         ),
         # Move Group
-        *generate_move_group_launch(moveit_config).entities,
+        Node(
+            package="moveit_ros_move_group",
+            executable="move_group",
+            output="screen",
+            respawn=False,
+            parameters=move_group_params,
+            remappings=[
+                ('/controller_manager/list_controllers', '/ada/controller_manager/list_controllers'),
+            ],
+        ),
         # RViz
         GroupAction(
             actions=generate_moveit_rviz_launch(moveit_config).entities,
