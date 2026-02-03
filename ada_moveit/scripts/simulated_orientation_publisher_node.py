9e201e002722ff6f09d3949ef9cbcdb301009fd1
Eddie Ni <eddieni06@gmail.com>
2026-02-02 17:39:36 -0800

    fixed pre-commit issues

--- ada_moveit/scripts/simulated_orientation_publisher_node.py
+++ ada_moveit/scripts/simulated_orientation_publisher_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # -*- coding: utf-8 -*-
-# Copyright (c) 2025, Personal Robotics Laboratory
+# Copyright (c) 2025-2026, Personal Robotics Laboratory
 # License: BSD 3-Clause. See LICENSE.md file in root directory.
 
 # License: BSD 3-Clause. See LICENSE.md file in root directory.
--- ada_moveit/scripts/unified_joint_state_publisher.py
+++ ada_moveit/scripts/unified_joint_state_publisher.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2025, Personal Robotics Laboratory
+#!/usr/bin/env python3
+# Copyright (c) 2025-2026, Personal Robotics Laboratory
 # License: BSD 3-Clause. See LICENSE.md file in root directory.
 
 import rclpy
